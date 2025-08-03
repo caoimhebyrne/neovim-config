0e84ca6d98d5317e9991e4f0420822d710307eb8
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2025-08-03 21:29:16 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## neovim-config
+
+This is my neovim configuration :)
+
+### License
+
+There is no license, this is my configuration files, do whatever you want with them :)
+
+
---
+++ lua/caoimhe/init.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+vim.opt.background = "dark"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true 
+vim.opt.wrap = false
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
+
+vim.keymap.set("n", "<leader>h", vim.diagnostic.open_float)
+vim.keymap.set("n", "F", vim.lsp.buf.format)
